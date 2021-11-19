b46d7b15acaaa6f465e95fc1833c3e9ab8b6bc8a
Emanuele Gennuso <emanuele.gennuso@rai.it>
2021-11-19 15:21:21 +0100

    .

--- src/AffiliateMarketing/Request.php
+++ src/AffiliateMarketing/Request.php
@@ -4,15 +4,17 @@ namespace BLZ_AFFILIATION\AffiliateMarketing;
 
 class Request {
 
+    private $marketplace;
     private $keyword;
     private $code;
     private $min_price;
 
     function __construct( $args ) 
     {
+        $this->marketplace = isset( $args['marketplace'] ) ? $args['marketplace'] : 'amazon';
         $this->keyword = isset( $args['keyword'] ) ? $args['keyword'] : '__default__';
         $this->code    = isset( $args['code'] ) ? $args['code'] : null;
-        $this->min_price    = isset( $args['min_price'] ) ? $args['min_price'] : null;
+        $this->min_price    = isset( $args['min_price'] ) ? $args['min_price'] : '20';
     }
 
     /**
@@ -25,7 +27,20 @@ class Request {
         return $this->code != null;
     }
 
+
     public function getKeyword() { return $this->keyword; } 
-    public function getCode()    { return ( $this->code ) ? $this->code : ''; } 
-    public function getMinPrice()    { return ( $this->min_price ) ? $this->min_price : 20; } 
+    public function getCode()    { return  $this->code; } 
+    public function getMinPrice()    { return $this->min_price; } 
+    private function getMarketplace(String $marketplace) {
+
+        $marketplace = [
+            "ebay" => "Ebay" ,
+            "ebay_used" => "EbayUsed", 
+            "trovaprezzi" => "Trovaprezzi",  
+            "amazon" => "Amazon", 
+            "custom" => "custom"
+        ];
+        return $MarketPlaceMap[$marketplace];
+    }
+
 }
