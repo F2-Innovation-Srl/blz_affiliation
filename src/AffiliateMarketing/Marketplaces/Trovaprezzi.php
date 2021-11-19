c6a3b2eb29f8b0913f58af81dd1d005e3e3deabe
Emanuele Gennuso <emanuele.gennuso@rai.it>
2021-11-19 17:34:39 +0100

    .

--- src/AffiliateMarketing/Marketplaces/Trovaprezzi.php
+++ src/AffiliateMarketing/Marketplaces/Trovaprezzi.php
@@ -8,12 +8,12 @@ class Trovaprezzi extends Marketplace {
     public function getQueryURL() {
 
         $query = $this->request->hasCode() ? $this->request->getCode() : $this->request->getKeyword();
-
+        $code_suffix = $this->request->hasCode() ? '&code=true' : "";
         /// sostituisce i valori nella query
         $apiQuery = str_replace(['{{ query }}', '{{ marketplace }}'], [ urldecode( $query ), $this->name], $this->apiQuery );
         $apiParams = str_replace('{{ min_price }}', $this->request->getMinPrice() , $this->apiParams );
         
-        return $this->apiBase . $apiQuery . $apiParams . $this->request->hasCode() ? "&code=true" : "";
+        return $this->apiBase . $apiQuery . $apiParams . $code_suffix;
     }
 }
 
