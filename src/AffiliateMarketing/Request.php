8ad00efbb839abb68f38d72ca75be86a88f1abad
Emanuele Gennuso <emanuele.gennuso@rai.it>
2021-11-19 17:09:10 +0100

    .

--- src/AffiliateMarketing/Request.php
+++ src/AffiliateMarketing/Request.php
@@ -13,9 +13,10 @@ class Request {
     {
         $this->marketplace  = isset( $args['marketplace'] ) ? $args['marketplace'] : 'amazon';
         $this->keyword      = isset( $args['keyword'] ) ? $args['keyword'] : '__default__';
-        $this->code         = isset( $args['code'] ) ? $args['code'] : null;
+        $this->code         = isset( $args['code'] ) ? $args['code'] : (isset( $args['asins'] ) ? $args['asins'] : null); //CONTROLLO ASIN PER RETROCOMPATIBILITA'
         $this->min_price    = isset( $args['min_price'] ) ? $args['min_price'] : '20';
-
+        $this->content      = isset( $args['text'] ) ? $args['text'] : '';
+        $this->tracking_id  = isset( $args['tracking_id'] ) ? $args['tracking_id'] : (isset( $args['store'] ) ? $args['store'] : null); //CONTROLLO store PER RETROCOMPATIBILITA'
     }
 
     /**
@@ -28,10 +29,11 @@ class Request {
         return $this->code != null;
     }
 
-
-    public function getKeyword() { return $this->keyword; } 
-    public function getCode()    { return  $this->code; } 
+    public function getKeyword()     { return $this->keyword; } 
+    public function getCode()        { return  $this->code; } 
     public function getMinPrice()    { return $this->min_price; } 
+    public function geContent()      { return $this->content; } 
+    public function getTrackingId()  { return $this->tracking_id; } 
     public function getMarketplace() {
 
         $MarketPlaceMap = [
