dca09986b4b7c655291bbb0b4392ca010654b3dc
Emanuele Gennuso <emanuele.gennuso@rai.it>
2021-11-28 23:18:00 +0100

    .

--- src/assets/js/admin.js
+++ src/assets/js/admin.js
@@ -7,14 +7,14 @@
         document.addEventListener("DOMContentLoaded", function() {
 
             var deleteButtons = document.querySelectorAll(".delete")
-            for (i = 0; i < deleteButtons.length; i++) {
+            for (let i = 0; i < deleteButtons.length; i++) {
                 deleteButtons[i].addEventListener('click', function() {
                     document.querySelector("#hidden_for_delete").value=this.dataset.id;
                 });
             }
         
             var attivatoreLists = document.querySelectorAll(".attivatore")
-            for (i = 0; i < attivatoreLists.length; i++) {
+            for (let i = 0; i < attivatoreLists.length; i++) {
                 attivatoreLists[i].addEventListener('change', function() {
                     document.getElementById('GaTrakingIdSettings').submit();
                 });
