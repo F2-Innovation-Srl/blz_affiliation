3f3b3c42b14b6c9222a9f28bae3e86c58ce20d59
Emanuele Gennuso <emanuele.gennuso@rai.it>
2021-11-28 23:10:45 +0100

    .

--- src/AdminUserInterface/Settings/Modules/Fields/Text.php
+++ src/AdminUserInterface/Settings/Modules/Fields/Text.php
@@ -16,8 +16,8 @@ class Text extends Field {
                 return '<input type="text" name="'.$this->name.'" value="'.$this->value.'" />';
                 break;
             case "boolean":
-                foreach (["true","false"] as $options)
-                    $output .= 'SI <input type="radio" name="'.$this->name.'" '.( ($this->value == $options) ? "checked" : "").' value="'.$options.'" />';
+                foreach (["SI" => "true","NO" => "false"] as $key => $options)
+                    $output .= $key.' <input type="radio" name="'.$this->name.'" '.( ($this->value == $options) ? "checked" : "").' value="'.$options.'" />';
                 return $output;
                 break;
             case "hidden":
