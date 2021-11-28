c7605f94dac5f8237c08437ddc0056c1e092e8f0
Emanuele Gennuso <emanuele.gennuso@rai.it>
2021-11-28 02:29:28 +0100

    .

--- src/assets/js/admin.js
+++ src/assets/js/admin.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var attivatoreLists = document.querySelectorAll(".attivatore")
     for (i = 0; i < attivatoreLists.length; i++) {
         attivatoreLists[i].addEventListener('change', function() {
-            document.getElementById("GaTrakingIdSettings").submit();
+            document.GaTrakingIdSettings.submit();
         });
     }
     
