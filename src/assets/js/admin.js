979d078a26312614b4fdd1f0aa7768e97106ec4d
Emanuele Gennuso <emanuele.gennuso@rai.it>
2021-11-28 23:23:23 +0100

    .

--- src/assets/js/admin.js
+++ src/assets/js/admin.js
@@ -1,34 +1,24 @@
-(function ($, root, undefined) {
-	
+(function () {
 
 		'use strict';
 		
-		// DOM ready, take it away
-        $(function () {
-		
-            'use strict';
-            
-            // DOM ready, take it away
-            var deleteButtons = (document.querySelectorAll(".delete")) ? document.querySelectorAll(".delete") : 0
-            for (let i = 0; i < deleteButtons.length; i++) {
-                deleteButtons[i].addEventListener('click', function() {
-                    document.querySelector("#hidden_for_delete").value=this.dataset.id;
-                });
-            }
-        
-            var attivatoreLists = (document.querySelectorAll(".attivatore")) ? document.querySelectorAll(".delete") : 0
-            for (let i = 0; i < attivatoreLists.length; i++) {
-                attivatoreLists[i].addEventListener('change', function() {
-                    document.getElementById('GaTrakingIdSettings').submit();
-                });
-            }
-            
-        });
-        
         document.addEventListener("DOMContentLoaded", function() {
 
-          
-        });
+          // DOM ready, take it away
+          var deleteButtons = (document.querySelectorAll(".delete")) ? document.querySelectorAll(".delete") : 0
+          for (let i = 0; i < deleteButtons.length; i++) {
+              deleteButtons[i].addEventListener('click', function() {
+                  document.querySelector("#hidden_for_delete").value=this.dataset.id;
+              });
+          }
+      
+          var attivatoreLists = (document.querySelectorAll(".attivatore")) ? document.querySelectorAll(".delete") : 0
+          for (let i = 0; i < attivatoreLists.length; i++) {
+              attivatoreLists[i].addEventListener('change', function() {
+                  document.getElementById('GaTrakingIdSettings').submit();
+              });
+          }
 
-	
-})(jQuery, this);
+        });
+        
+});
