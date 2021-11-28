2b806298354853079057caefc160fd1d76a21daf
Emanuele Gennuso <emanuele.gennuso@rai.it>
2021-11-28 23:16:05 +0100

    .

--- src/assets/js/admin.js
+++ src/assets/js/admin.js
@@ -3,19 +3,21 @@
 		'use strict';
 		
 		// DOM ready, take it away
-        var deleteButtons = document.querySelectorAll(".delete");
-        for (i = 0; i < deleteButtons.length; i++) {
-            deleteButtons[i].addEventListener('click', function() {
-                document.querySelector("#hidden_for_delete").value=this.dataset.id;
-            });
-        }
+        document.addEventListener("DOMContentLoaded", function() {
 
-        var attivatoreLists = document.querySelectorAll(".attivatore");
-        for (i = 0; i < attivatoreLists.length; i++) {
-            attivatoreLists[i].addEventListener('change', function() {
-                document.getElementById('GaTrakingIdSettings').submit();
-            });
-        }
-
+            var deleteButtons = document.querySelectorAll(".delete")
+            for (i = 0; i < deleteButtons.length; i++) {
+                deleteButtons[i].addEventListener('click', function() {
+                    document.querySelector("#hidden_for_delete").value=this.dataset.id;
+                });
+            }
+        
+            var attivatoreLists = document.querySelectorAll(".attivatore")
+            for (i = 0; i < attivatoreLists.length; i++) {
+                attivatoreLists[i].addEventListener('change', function() {
+                    document.getElementById('GaTrakingIdSettings').submit();
+                });
+            }
+        });
 	
 });
