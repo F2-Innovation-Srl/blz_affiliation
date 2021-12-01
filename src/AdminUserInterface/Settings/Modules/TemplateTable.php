ec6c10674b8373da5133afc503f74db3433cbe7d
Emanuele Gennuso <emanuele.gennuso@rai.it>
2021-12-01 19:02:17 +0100

    .

--- src/AdminUserInterface/Settings/Modules/TemplateTable.php
+++ src/AdminUserInterface/Settings/Modules/TemplateTable.php
@@ -19,13 +19,15 @@ class TemplateTable {
         $this->current = $current;
 
         $row = $this->getAndSetRow($option_name);
-
+        $hiddenGA = (empty($this->current["tab"]["ga_event_template"]) ) ? "hidden" : "text"; 
+        $hiddenTrack = (empty($this->current["tab"]["tracking_id"]) ) ? "hidden" : "text"; 
+       
         $this->row =  [
             (new Fields\Text($option_name."_active",$row["active"],"boolean")),
             (new Fields\Text($option_name."_ga_default",$row["ga_default"],"text")),
             (new Fields\Text($option_name."_trk_default",$row["trk_default"],"text")),
-            (new Fields\Text($option_name."_website_ga",$row["website_ga"],"text")),
-            (new Fields\Text($option_name."_website_trk",$row["website_trk"],"text")),
+            (new Fields\Text($option_name."_website_ga",$row["website_ga"],$hiddenGA)),
+            (new Fields\Text($option_name."_website_trk",$row["website_trk"],$hiddenTrack)),
             (new Fields\Text('',"Aggiorna valori","button"))
         ];
     }
@@ -38,24 +40,35 @@ class TemplateTable {
         <div><h2>Settings</h2></div>
         <table>
             <tr valign="top" style="text-align:left">
-                <th>Attivo</th>   <th>Valore TRK_ID Default</th>  <th>Valore GA Default</th>  <th>{website} TRK_ID </th>   <th>{website} GA </th>      
+                <th>Attivo</th>   <th>Valore TRK_ID Default</th>  <th>Valore GA Default</th>  
+                <?php if (!empty($this->current["tab"]["ga_event_template"])) : ?>
+                <th>{website} TRK_ID </th>
+                <?php endif;?>    
+                <?php if (!empty($this->current["tab"]["tracking_id"])) : ?>
+                <th>{website} GA </th>  
+                <?php endif;?>   
             </tr>
             <tr valign="top" style="text-align:left">
             <?php foreach( $this->row as $field )  echo "<td>" .$field->render() ."</td>"; ?>
             </tr>
         </table>
+        <?php if (!empty($this->current["tab"]["ga_event_template"]) || !empty($this->current["tab"]["tracking_id"]) ) :?>
        <div><h2>Template</h2></div>
         <table>
+            <?php if (!empty($this->current["tab"]["ga_event_template"])) : ?>
             <tr valign="top" style="text-align:left">
                <th>GA EVENT</th> 
                <td><input style="width:450px" type="text" readonly value="<?php echo $this->current["tab"]["ga_event_template"];?>"></td>
             </tr>
+            <?php endif;
+            if (!empty($this->current["tab"]["tracking_id"])) : ?>
             <tr valign="top" style="text-align:left">
                <th>TRACKING ID</th>
                <td><input style="width:450px" type="text" readonly value="<?php echo $this->current["tab"]["tracking_id"];?>"></td>
             </tr>
+            <?php endif;?>
         </table>
-
+        <?php endif;?>
         
     <?php
        
