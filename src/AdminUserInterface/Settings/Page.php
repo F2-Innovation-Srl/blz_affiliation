55d5133bdf4dc21c51b764367d7e86956bde56b7
Emanuele Gennuso <emanuele.gennuso@rai.it>
2021-12-06 17:32:10 +0100

    .

--- src/AdminUserInterface/Settings/Page.php
+++ src/AdminUserInterface/Settings/Page.php
@@ -12,13 +12,15 @@ class Page {
 	 */
 	function __construct() {
         
- 
-        add_action( 'wp_enqueue_scripts', [ $this, 'mytheme_enqueue_typekit' ] );
+        add_action('init', [ $this, 'custom_enqueue' ]);
+       
 	}
 
-	function mytheme_enqueue_typekit() { 
-        wp_add_inline_script( 'mytheme-typekit', 'var blz-affiliation-ga=\'GA_sssss\'' );
+	function custom_enqueue() { 
+        add_action( 'wp_enqueue_scripts', [ $this, 'mytheme_enqueue_typekit' ] );
+        
     }
-
-    
+    function mytheme_enqueue_typekit() { 
+         wp_add_inline_script( 'mytheme-typekit', 'var blz-affiliation-ga=\'GA_sssss\'' );
+    }
 }
