1e0590a8e3525a6da3e20c75558f9f8c3743069a
Andrea Marzilli <a.marzilli@f2innovation.com>
2021-12-10 14:32:51 +0100

    .

--- src/AdminUserInterface/Settings/Modules/Fields/Label.php
+++ src/AdminUserInterface/Settings/Modules/Fields/Label.php
@@ -6,39 +6,69 @@ namespace BLZ_AFFILIATION\AdminUserInterface\Settings\Modules\Fields;
  */
 class Label extends Field {
 
+    private $settings = [
+        'GA'     => [ 'tab' => 'ga_event_template' ],
+        'TRK_ID' => [ 'tab' => 'tracking_id' ]
+    ];
+    
+    protected $output =
+    <<<HTML
+        <select class="label" name="{{ name }}">
+            <option value="">Seleziona una {{ type }} label</option>
+            {{ options }}
+        </select>
+    HTML;
+
+    protected $option = '<option value="{{ value }}"{{ selected }}>{{ label }}</option>';   
+
+
     /// viene richiamata dal costruttore
     public function Create() {
-        $labels = [];
-        if (($this->type == "GA" && !empty($this->params["tab"]["ga_event_template"])) OR ($this->type == "TRK_ID" && !empty($this->params["tab"]["tracking_id"]))){
-            $output = '<select class="label" name="'.$this->name.'"><option value="">Seleziona una '.$this->type.' label</option>';
-            switch ($this->type) {
-                case "GA":
-                    $labels = $this->getLabels($this->params["tab"]["ga_event_template"],"{","}");
-                    break;
-                case "TRK_ID":
-                    $labels = $this->getLabels($this->params["tab"]["tracking_id"],"{","}");
-                    break;
-            } 
-            foreach($labels as $label)
-                $output.= '<option value="'.$label.'" '.(($this->value == $label) ? "selected" : "").' >'.$label.'</option>';    
-            $output.= '</select>';
-        }else{
-            $output= '<input type="hidden" id="'.$this->name.'" name="'.$this->name.'" value="'.$this->value.'" />'; 
+        
+        $typeGA    = ( $this->type == "GA"     && !empty( $this->params["tab"]["ga_event_template"] ));
+        $typeTRKID = ( $this->type == "TRK_ID" && !empty( $this->params["tab"]["tracking_id"] ));
+
+        if( !$typeGA && !$typeTRKID ) {
+
+            return '<input type="hidden" id="'.$this->name.'" name="'.$this->name.'" value="'.$this->value.'" />'; 
         }
-        return $output;
+        
+        $labels = $this->getLabels( $this->params["tab"][ $this->settings[ $this->type][ 'tab' ] ], "{", "}" );
+        
+        $options = array_map( function( $label ) {
+
+            $selected = ( $this->value == $label ) ? ' selected ' : '';
+
+            return str_replace(
+                [ '{{ value }}'.'{{ label }}','{{ selected }}' ],
+                [ $label, $label, $selected ],
+                $this->option
+            );
+            
+        }, $labels);
+        
+        return str_replace(
+            [ '{{ name }}', '{{ type }}', '{{ options }}' ],
+            [ $this->name, $this->type, $options ],
+            $this->output
+        );
     }
 
-    private function getLabels($str, $startDelimiter, $endDelimiter) {
-        $contents = [];
-        $startFrom = $contentStart = $contentEnd = 0;
-        while (false !== ($contentStart = strpos($str, $startDelimiter, $startFrom))) {
-          $contentStart += strlen($startDelimiter);
-          $contentEnd = strpos($str, $endDelimiter, $contentStart);
-          if (false === $contentEnd)  break;
-          $content = substr($str, $contentStart, $contentEnd - $contentStart);
-          if ("website" != $content) $contents[] = $content;
-          $startFrom = $contentEnd + strlen($endDelimiter);
-        }
-        return $contents;
+    
+    private function getLabels(string $str, string $startDelimiter, string $endDelimiter) {
+    
+        $regex = '/' .$startDelimiter . '(.*?)'. $endDelimiter .'/';
+        
+        /// prende tutti i termini racchiusi nei delimitatori
+        preg_match_all( $regex, $str, $matches);
+
+        /// rimuove gli spazi intorno ai terms
+        $terms = isset( $matches[1]) ? array_map( function( $term ) {  return trim( $term );  }, $matches[1] ) : [];
+
+        /// rimuove 'website' se esiste;
+        $terms = array_values( array_filter( $terms, function( $term ) { return $term != 'website'; }) );
+        
+        return $terms;
     }
+
 }
