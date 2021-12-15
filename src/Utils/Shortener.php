4d3d20baceb8f948fc6f59db53705dbb58bcb13b
Emanuele Gennuso <emanuele.gennuso@rai.it>
2021-12-15 18:05:41 +0100

    .

--- src/Utils/Shortener.php
+++ src/Utils/Shortener.php
@@ -33,7 +33,7 @@ class Shortener {
         $short_data = json_decode(curl_exec($curl));
         //print_r($short_data);
         curl_close($curl);
-        return ($short_data->shorturl) ? ($short_data->shorturl) : $link;
+        return ($short_data->shorturl) ? (urldecode($short_data->shorturl)) : $link;
 
     }
 
