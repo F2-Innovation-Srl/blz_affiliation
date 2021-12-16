65febc68971e1fea02609fee93bdd48c706a5ecb
Andrea Marzilli <a.marzilli@f2innovation.com>
2021-12-16 16:18:15 +0100

    .

--- src/AdminUserInterface/Settings/AdminPage.php
+++ src/AdminUserInterface/Settings/AdminPage.php
@@ -22,13 +22,14 @@ class AdminPage {
      * Create admin menu 
     **/
 	public function adminMenu() {
+        
         add_menu_page(CONFIG["plugin_name"], CONFIG["plugin_name"], 'manage_options', CONFIG["plugin_suffix"]);
+        
         foreach(CONFIG["Items"] as $item){
             $className = "BLZ_AFFILIATION\\AdminUserInterface\\Settings\\". $item["class_name"];
             $class = new $className;
-            add_submenu_page(CONFIG["plugin_suffix"], $item["name"],$item["name"], 'manage_options', $item["suffix"], [ $class, 'render']);
+            add_submenu_page( CONFIG["plugin_suffix"], $item["name"], $item["name"], 'manage_options', $item["suffix"], [ $class, 'render'] );
         }
-            
 	}
 
     public static function custom_enqueue()
@@ -44,6 +45,10 @@ class AdminPage {
                 true
             );
         } else {
+
+            /// dipendenze js per tracciamento
+            wp_enqueue_script('blz-affiliation-tracker',   PLUGIN_URI ."src/assets/js/libs/tracker.min.js",[], PLUGIN_VERSION,true);
+            wp_enqueue_script('blz-affiliation-activator', PLUGIN_URI ."src/assets/js/affiliate-link-activator.js",["blz-affiliation-tracker"], PLUGIN_VERSION,true);
 
             # enqueue CSS
             wp_enqueue_style( 'blz-affiliation-grid-css', PLUGIN_URI ."src/assets/css/flex-grid-lite.css", [],  PLUGIN_VERSION, 'all' );
