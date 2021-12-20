b84337f32668a5e153d87d9e16b800a75cd3ebe8
Emanuele Gennuso <emanuele.gennuso@rai.it>
2021-12-20 15:09:27 +0100

    .

--- src/Rendering/AffiliateTable.php
+++ src/Rendering/AffiliateTable.php
@@ -63,7 +63,7 @@ class AffiliateTable {
                 "text"             => $row['text'],
                 "rating"           => $row['rating'],
                 "cta"              => $row['cta'],
-                "link"             => ($post_data["is_amp"]  == "false") ?  $row['link'] : $row['link_amp'],
+                "link"             => ($post_data->is_amp  == "false") ?  $row['link'] : $row['link_amp'],
                 "ga_event"         => $settings->getGAEvent()
             ];
 
