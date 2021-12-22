eb49449d9b1a7b806f3df1bfbc242947ac1ef98c
Emanuele Gennuso <emanuele.gennuso@rai.it>
2021-12-22 13:04:51 +0100

    .

--- src/AdminUserInterface/Settings/Tables/ActivationTableImport.php
+++ src/AdminUserInterface/Settings/Tables/ActivationTableImport.php
@@ -14,16 +14,16 @@ class ActivationTableImport extends Table {
         $this->title = "Importa da altra tabella di attivazione";
 
         $this->rows[] =  [
-            "Attivatori" => (new Fields\Import($this->option_name."_activation_import","","",["current" => $this->option_name])),
+            "Attivatori" => (new Fields\Import($this->option_name,"","",["current" => $this->option_name])),
             "Azioni" => (new Fields\Text($this->option_name."_new",'Importa',"button"))
         ];
     }
 
     protected function getAndSetRows(){
-        
-        if (isset( $_POST[$this->option_name."_activation_import"])  && !empty($_POST[$this->option_name."_activation_import"]) ){
-            $activationRows = get_option( $_POST[$this->option_name."_activation_import"]);    
-            update_option($this->option_name,$activationRows);
+       
+        if (isset( $_POST[$this->option_name])  && !empty($_POST[$this->option_name]) ){
+            $activationRows = get_option( $_POST[$this->option_name]);    
+            //update_option($this->option_name,$activationRows);
         }
     }
 }
