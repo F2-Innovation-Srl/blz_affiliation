112adb59d0123d22b981ce4bf8933831c8302ae0
Emanuele Gennuso <emanuele.gennuso@rai.it>
2021-12-22 12:57:36 +0100

    .

--- src/AdminUserInterface/Settings/Tables/ActivationTableImport.php
+++ src/AdminUserInterface/Settings/Tables/ActivationTableImport.php
@@ -7,58 +7,22 @@ use BLZ_AFFILIATION\AdminUserInterface\Settings\Tables\Fields;
  *
  * @package BLZ_AFFILIATION
  */
-class ActivationTableImport {
+class ActivationTableImport extends Table {
 
-    private $rows;
-    private $title = "Importa da altra tabella di attivazione";
-    private $output = [
-        "table" => 
-            <<<HTML
-            <div><h2>{{ title }}</h2></div>
-            <table>
-               <thead><tr valign="top" style="text-align:left">{{ headings }}</tr></thead>
-                <tbody>{{ rows }}</thead>
-            </table>
-            HTML
-    ];
-    
-	/**
-	 * AttivazioneRow constructor.
-	 */
-	function __construct($option_name) {
-        $this->getAndSetRows($option_name);
+	protected function getTableFields($rows) {
+
+        $this->title = "Importa da altra tabella di attivazione";
+        
         $this->rows[] =  [
             "Attivatori" => (new Fields\Import($option_name."_activation_import","","",["current" => $option_name])),
             "Azioni" => (new Fields\Text($option_name."_new",'Importa',"button"))
         ];
     }
 
-	/**
-     * Print page if have correct permission
-    **/
-    public function render(){
-
-        $headings = array_reduce( array_keys( $this->rows[0] ), function( $cols, $key ) { 
-
-            $cols .= "<th>$key</th>";
-            return $cols;
-        } );
-        
-        foreach ($this->rows as $row)
-        $rows.= '<tr valign="top">'.array_reduce( $row, function( $cols, $field ) { 
-
-            $cols .= '<td >' . $field->render() . '</td>';
-            return $cols;
-        } ).'</tr>';
-        
-        return str_replace([ '{{ title }}', '{{ headings }}', '{{ rows }}' ], [$this->title, $headings, $rows ], $this->output["table"] );       
-
-    }
-
-    private function getAndSetRows($option_name){
+    protected function getAndSetRows($option_name){
         
         if (isset( $_POST[$option_name])  && !empty($_POST[$option_name]) ){
-            $activationRows = get_option( $_POST[$option_name]);    
+            $activationRows = get_option( $_POST[$option_name."_activation_import"]);    
             update_option($option_name,$activationRows);
         }
     }
