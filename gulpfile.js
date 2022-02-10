0d2206f1363ce0f1dc71dae207d0b8e0dc210b4a
Andrea Marzilli <a.marzilli@f2innovation.com>
2022-02-10 10:58:13 +0100

    added gulp for js deps

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp'),                     // base
+    
+    //fs       = require('fs'),                   
+    //file     = require('gulp-file'),            // creates a new file
+
+    del = require('del');                  // deletes old dist files
+
+    
+
+var buildFolder = 'src/assets/',    
+    scriptsBuildFolder, jsLibBuildFolder;
+
+const setFolders = (folder = 'static/') => {
+    buildFolder = folder;    
+    scriptsBuildFolder = buildFolder + 'js/';
+    jsLibBuildFolder  = scriptsBuildFolder + '/libs';
+}
+
+setFolders(buildFolder);
+
+// task to delete each file in the dist directory
+gulp.task('clean', () => del([ jsLibBuildFolder ]));
+
+
+/**
+ * Aggiorna la cartella delle librerie js
+ */
+ gulp.task('scripts:lib', function() {
+
+    const libs_src = [
+        'node_modules/tracker/dist/tracker.min.js'
+    ];
+
+    return gulp.src( libs_src )
+        .pipe( gulp.dest( jsLibBuildFolder ) );
+});
+
+
+
+gulp.task( 'common-chain', gulp.series('clean', 'scripts:lib') );
+
+gulp.task( 'default',
+    gulp.series( 'common-chain' )
+);
+
