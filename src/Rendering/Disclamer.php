feb0fa68cf93bca634e68839adfbe397d03aae91
Emanuele Gennuso <emanuele.gennuso@rai.it>
2022-05-24 16:58:25 +0200

    fix

--- src/Rendering/Disclamer.php
+++ src/Rendering/Disclamer.php
@@ -1,6 +1,7 @@
 <?php
 namespace BLZ_AFFILIATION\Rendering;
-
+use BLZ_AFFILIATION\AffiliateMarketing\Request;
+use BLZ_AFFILIATION\Rendering\Settings\SettingsData;
 use BLZ_AFFILIATION\AdminUserInterface\Settings\Config;
 /**
  * Aggiunge CSS che usa parametri presi dai settings
@@ -17,7 +18,7 @@ class Disclamer {
         $request = new Request([]);
         /// inizializzo i settingsData 
         $SettingsData = new SettingsData("blz_disclamer",$request);
-           
+          
         $this->text = $SettingsData->getGAEvent();
         // Add the custom columns to the posts post type:
         add_filter( 'the_content', [ $this, 'add'], 99 );        
@@ -27,9 +28,10 @@ class Disclamer {
         
         $config = Config::loadSettings();
         if ($config->is_affiliation_page == "true")
-            return $content . $this->text;
-        else    
+            return $content. "<p class='blz_affiliation_disclamer'>" .$this->text."</p>";
+        else   
             return $content;
+        
     }
 
 
