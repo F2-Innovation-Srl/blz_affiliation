1338ba8540c9a16675a1f9cfefc4f89a52c82d8c
Emanuele Gennuso <emanuele.gennuso@rai.it>
2022-05-24 17:08:55 +0200

    .

--- src/Rendering/Disclamer.php
+++ src/Rendering/Disclamer.php
@@ -13,25 +13,26 @@ class Disclamer {
 
 	public function __construct() {
 
-        //$text = get_option( "blz-affiliation-settings-disclamer" );
-        /// prende la request
-        $request = new Request([]);
-        /// inizializzo i settingsData 
-        $SettingsData = new SettingsData("blz_disclamer",$request);
-          
-        $this->text = $SettingsData->getGAEvent();
+        
+        $config = Config::loadSettings();
+        
+        if ($config->is_affiliation_page == "true")){
+            $text = get_option( "blz-affiliation-settings-disclamer" );
+            $this->text = (!empty($text["disclamer"])) ? "<p class='blz_affiliation_disclamer'>" . $text["disclamer"] . "</p>" : "";
+        }else{
+            /// prende la request
+            $request = new Request([]);
+            /// inizializzo i settingsData 
+            $SettingsData = new SettingsData("blz_disclamer",$request);
+            $this->text = $SettingsData->getGAEvent();
+        }
+        
         // Add the custom columns to the posts post type:
         add_filter( 'the_content', [ $this, 'add'], 99 );        
     }
 
 	function add($content) { 
-        
-        $config = Config::loadSettings();
-        if ($config->is_affiliation_page == "true")
             return $content. "<p class='blz_affiliation_disclamer'>" .$this->text."</p>";
-        else   
-            return $content;
-        
     }
 
 
