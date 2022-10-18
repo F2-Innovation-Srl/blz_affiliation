43e7d22aeb468e66707289930f089b435c9cdc26
Andrea Marzilli <a.marzilli@f2innovation.com>
2022-10-18 15:35:14 +0200

    generic button patch

--- src/Rendering/AffiliateGenericButton.php
+++ src/Rendering/AffiliateGenericButton.php
@@ -6,10 +6,10 @@ use BLZ_AFFILIATION\AffiliateMarketing\Request;
 use BLZ_AFFILIATION\Rendering\Settings\SettingsData;
 use BLZ_AFFILIATION\Utils\Helper;
 use BLZ_AFFILIATION\Utils\Shortener;
+
+
 /**
- * 
- * Ritorna i dati della tabella di affiliazione nella pagina
- * 
+ * Stampa un link di tipo button 
  */
 class AffiliateGenericButton {
 
@@ -27,47 +27,58 @@ class AffiliateGenericButton {
      *     
      */
     public function printAffiliateLink( $atts, $content, $tag ) {
-        
-       
 
         // recupera i pattern per ogni 
         /// tipologia di link da sostituire
-        $patterns = array_map( function( $patternClass ) use ($atts){
+        $patterns = array_map( function( $patternClass ) use ($atts) {
+
             $patternClass = 'BLZ_AFFILIATION\\Rendering\\ParseLinkAndRender\\Patterns\\' . $patternClass;
             return new $patternClass("<a href=\"".$atts["url"]."\">");
         },  Helper::getMarketplacePatterns() );
 
-        /// cerco tra i patterns il mio link 
-        foreach( $patterns as $pattern ) 
-            foreach( $pattern->data as $linkData ) 
-                if ($linkData->url) $this->linkData = $linkData;
+       
+        
+        /// cerca il link tra i pattern
+        foreach( $patterns as $pattern ) {
+            foreach( $pattern->data as $linkData ) {
 
+                if ( $linkData->url == $atts['url'] ) $this->linkData = $linkData;
+            }                
+        }
+            
+        $atts[ "marketplace" ] = $this->linkData->marketplace;
 
-        $atts["marketplace"] = $this->linkData->marketplace;
+        /// prende la request
+        $this->request = new Request( $atts );
 
+        /// inizializza i settingsData 
+        $settingsData = new SettingsData("parseLinkAndRender",$this->request);
 
-        /// prendo la request
-        $this->request = new Request($atts);
-        /// inizializzo i settingsData 
-        $SettingsData = new SettingsData("parseLinkAndRender",$this->request);
-        return $this->FillTemplate( $SettingsData->getGAEvent(), $SettingsData->getTrackingID(), $SettingsData->getTemplate() );
-
+        return $this->FillTemplate( $settingsData->getGAEvent(), $settingsData->getTrackingID(), $settingsData->getTemplate() );
     }
 
 
     private function FillTemplate( $ga_event, $tracking, $template) {
       
         Helper::setAffiliationPage();
-        
-        $link = Helper::cleanParams($this->linkData->url);
-        
+    
+        $link = Helper::cleanParams( $this->linkData->url );
+
+        $link = empty( $link ) ? $this->linkData->url : $link;
+
+        /// questa patch non dovrebbe esistere a questo livello. Bisognerà fare un refactoring.
+        /// se manca il tracking id ed è un link amazon aggiunge il tag
+        if( $this->linkData->marketplace == 'amazon' && strpos($link, 'tracking') === false ) {
+
+            $link = $link . '?tag={tracking-id}';
+        }
+                
         $link = str_replace(['{tracking_id}','{tracking-id}','%7Btracking-id%7D'], $tracking, $link);
         
-        if (strpos($this->linkData->url, "ebay") === false) 
+        if (strpos($this->linkData->url, "ebay") === false) {
 
             $link = ( new Shortener )->generateShortLink( $link );
-
-        
+        }
              
         $content = $this->request->getContent();
 
