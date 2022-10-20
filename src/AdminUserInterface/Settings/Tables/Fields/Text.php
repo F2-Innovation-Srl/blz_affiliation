f23b344da65ae6e477194e3cc89c54f66bf0184f
Andrea Marzilli <a.marzilli@f2innovation.com>
2022-10-20 17:01:35 +0200

    text fix

--- src/AdminUserInterface/Settings/Tables/Fields/Text.php
+++ src/AdminUserInterface/Settings/Tables/Fields/Text.php
@@ -8,7 +8,11 @@ class Text extends Field {
 
     /// viene richiamata dal costruttore
     public function Create() {
+
+        $output = '';
+
         $hidden_field = isset($this->params["hidden_field"]) ? $this->params["hidden_field"] : "";
+
         switch ($this->type) {
             case "number":
                 return '<input type="number" name="'.$this->name.'" value="'.$this->value.'" />';
@@ -19,19 +23,22 @@ class Text extends Field {
             case "textarea":
                 return '<textarea rows="15" cols="110" name="'.$this->name.'" >'.$this->value.'</textarea>';
                 break;
+                
             case "boolean":
-                foreach (["NO" => "false","SI" => "true"] as $key => $options)
+                
+                foreach ( ["NO" => "false", "SI" => "true" ] as $key => $options ) {
+
                     $output .= $key.' <input type="radio" name="'.$this->name.'" '.( ($this->value == $options) ? "checked" : "").' value="'.$options.'" />';
+                }
+                    
                 return $output;
-                break;
+                
             case "hidden":
                 return '<input type="hidden" id="'.$this->name.'" name="'.$this->name.'" value="'.$this->value.'" />';
                 break;
             case "button":
                 return '<input type="submit" name="btnSubmit" data-id="'.$this->name.'" data-name="'.$hidden_field.'" class="button button-primary '.strtolower($this->value).'" value="'.$this->value.'" />';
                 break;
-        }
-        ?>
-        <?php
+        }        
     }
 }
