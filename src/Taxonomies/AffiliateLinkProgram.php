1d8fa665ed97f64f103bc08e493c58c2224738ac
Andrea Marzilli <a.marzilli@f2innovation.com>
2022-10-20 16:51:02 +0200

    .

--- src/Taxonomies/AffiliateLinkProgram.php
+++ src/Taxonomies/AffiliateLinkProgram.php
@@ -20,34 +20,57 @@ class AffiliateLinkProgram {
      * Initializer for setting up action handler
      */
     public static function init() {
-        add_action('init', [ get_called_class(), 'register_tax' ] );
-        add_action('save_post_program_stored_link', [ get_called_class(), 'save_tax' ], 20, 3 );
-        add_action('restrict_manage_posts', [ get_called_class(), 'tsm_filter_post_type_by_taxonomy']);
-        add_filter('parse_query', [ get_called_class(), 'tsm_convert_id_to_term_in_query']);
-        add_action('manage_program_stored_link_posts_columns', [ get_called_class(), 'add_column_to_listing_list']);
-        //add_action('manage_edit-program_stored_link_sortable_columns', [ get_called_class(), 'add_column_to_listing_list']);
-        add_action('manage_posts_custom_column', [ get_called_class(), 'show_column_for_listing_list'],10,2);
+        
+        add_action( 'init',                                     [ get_called_class(), 'register_tax'                     ] );
+        add_action( 'save_post_program_stored_link',            [ get_called_class(), 'save_tax'                         ], 20, 3 );
+        add_action( 'restrict_manage_posts',                    [ get_called_class(), 'tsm_filter_post_type_by_taxonomy' ] );        
+        add_action( 'manage_program_stored_link_posts_columns', [ get_called_class(), 'add_column_to_listing_list'       ] );        
+        add_action( 'manage_posts_custom_column',               [ get_called_class(), 'show_column_for_listing_list'     ], 10, 2 );
+        //add_action( 'manage_edit-program_stored_link_sortable_columns', [ get_called_class(), 'add_column_to_listing_list']);
 
+        add_filter( 'parse_query',                              [ get_called_class(), 'tsm_convert_id_to_term_in_query'  ] );
 
     }
 
-    
+    /**
+     * hook che scatta quando viene salvato un post di
+     * tipo program stored link
+     *
+     * @param int $post_id
+     * @param WP_Post $post
+     * @param array $update
+     * @return void
+     */
+    public static function save_tax( $post_id, $post, $update ) {
 
-    public static function save_tax($post_id, $post, $update ) {
+        if( $update ) {
 
-        if ($update)
-            foreach (self::$taxonomies as $taxonomy_slug => $taxonomy_name)
-                wp_delete_object_term_relationships( $post_id, $taxonomy_slug);
-        
-        $attr = shortcode_parse_atts($post->post_content);
-        foreach (self::$taxonomies as $taxonomy_slug => $taxonomy_name){
-                $key = str_replace("programs","program",str_replace("blz-affiliation-","",$taxonomy_slug));
-                if (isset($attr[$key])) {
-                    $term = get_term_by('slug', $attr[$key], $taxonomy_slug);
-                    if (isset($term->term_id)) wp_set_object_terms( $post_id, [$term->term_id,$term->parent], $taxonomy_slug);
-                }
+            /// rimuove dal post tutte le tassonomie
+            foreach( self::$taxonomies as $taxonomy_slug => $taxonomy_name) {
+
+                wp_delete_object_term_relationships( $post_id, $taxonomy_slug );
+            }
+        }
+            
+        /// prende lo shortcode
+        $attr = shortcode_parse_atts( $post->post_content );
+
+        foreach( self::$taxonomies as $taxonomy_slug => $taxonomy_name ) {
+            
+            $key = str_replace( "programs", "program", str_replace( "blz-affiliation-", "", $taxonomy_slug ) );
+            
+            if( isset( $attr[ $key ] ) ) {
+                
+                $term = get_term_by( 'slug', $attr[ $key ], $taxonomy_slug );
+                
+                if( isset( $term->term_id ) ) {
+
+                    wp_set_object_terms( $post_id, [ $term->term_id, $term->parent ], $taxonomy_slug );
+                } 
+            }
         }  
     }
+
 
     public static function register_tax() {
         
@@ -93,13 +116,21 @@ class AffiliateLinkProgram {
      * @link http://thestizmedia.com/custom-post-type-filter-admin-custom-taxonomy/
      */
     public static function tsm_filter_post_type_by_taxonomy() {
+
         global $typenow;
+
         $post_type = 'program_stored_link'; // change to your post type
-        foreach ( self::$taxonomies as $taxonomy => $taxonomy_name){
-            if ($typenow == $post_type) {
-                $selected      = isset($_GET[$taxonomy]) ? $_GET[$taxonomy] : '';
-                $info_taxonomy = get_taxonomy($taxonomy);
-                wp_dropdown_categories([
+
+        foreach ( self::$taxonomies as $taxonomy => $taxonomy_name) {
+
+            if( $typenow == $post_type ) {
+                
+                $selected      = isset( $_GET[ $taxonomy ]) ? $_GET[ $taxonomy ] : '';
+                
+                $info_taxonomy = get_taxonomy( $taxonomy );
+                
+                wp_dropdown_categories( [
+                    
                     'show_option_all' => sprintf( __( 'Tutti i %s', 'textdomain' ), $info_taxonomy->label ),
                     'taxonomy'        => $taxonomy,
                     'name'            => $taxonomy,
@@ -108,26 +139,35 @@ class AffiliateLinkProgram {
                     'show_count'      => true,
                     'hide_empty'      => false,
                     'hierarchical'    => true
-                ]);
+                ] );
             };
         }
     }
+
+
     /**
      * Filter posts by taxonomy in admin
      * @author  Mike Hemberger
      * @link http://thestizmedia.com/custom-post-type-filter-admin-custom-taxonomy/
      */
-    public static function tsm_convert_id_to_term_in_query($query) {
+    public static function tsm_convert_id_to_term_in_query( $query ) {
+        
         global $pagenow;
+        
         $post_type = 'program_stored_link'; // change to your post type
-        foreach ( self::$taxonomies as $taxonomy => $taxonomy_name){
+        
+        foreach ( self::$taxonomies as $taxonomy => $taxonomy_name) {
+            
             $q_vars    = &$query->query_vars;
+            
             if ( $pagenow == 'edit.php' && isset($q_vars['post_type']) && $q_vars['post_type'] == $post_type  && isset($q_vars[$taxonomy]) && is_numeric($q_vars[$taxonomy]) && $q_vars[$taxonomy] != 0 ) {
+
                 $term = get_term_by('id', $q_vars[$taxonomy], $taxonomy);
                 $q_vars[$taxonomy] = $term->slug;
             }
         }
     }
+
 
     public static function add_column_to_listing_list( $posts_columns ) {
         $new_posts_columns = [];
@@ -168,11 +208,20 @@ class AffiliateLinkProgram {
             usort($terms, function($a, $b) {return strcmp($b->parent, $a->parent);});
         return $terms;
     }
-    private static function createLink($term){
+
+
+    private static function createLink( $term ) {
+
         $link = "/wp/wp-admin/edit.php?post_type=program_stored_link";
-        foreach ( self::$taxonomies as $taxonomy_slug => $taxonomy_name)
-            if (isset($_GET[$taxonomy_slug]) || ($term->taxonomy == $taxonomy_slug)) 
+
+        foreach ( self::$taxonomies as $taxonomy_slug => $taxonomy_name) {
+
+            if ( isset( $_GET[$taxonomy_slug] ) || ( $term->taxonomy == $taxonomy_slug ) ) {
+
                 $link .= "&".$taxonomy_slug."=" . (($term->taxonomy == $taxonomy_slug) ? $term->term_id : $_GET[$taxonomy_slug]);
+            }                
+        }
+            
         return $link;
     }
 }
