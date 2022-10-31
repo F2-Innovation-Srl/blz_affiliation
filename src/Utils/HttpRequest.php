35e95865565520b87612faf524bacba5324cd88d
Andrea Marzilli <a.marzilli@f2innovation.com>
2022-10-31 17:24:11 +0100

    reduced requests timeout

--- src/Utils/HttpRequest.php
+++ src/Utils/HttpRequest.php
@@ -4,25 +4,31 @@ namespace BLZ_AFFILIATION\Utils;
 
 class HttpRequest {
 
-    static function getContent($url){
+    static function getContent( $url ) {
+
         $url = str_replace(" ", '%20', $url);
         $curl = curl_init();
-        curl_setopt_array($curl, array(
+
+        curl_setopt_array($curl, [
             CURLOPT_URL => $url,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => "",
             CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 5,
-            CURLOPT_CONNECTTIMEOUT => 5,
+            CURLOPT_TIMEOUT => 1,
+            CURLOPT_CONNECTTIMEOUT => 1,
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_SSL_VERIFYHOST => 0,
             CURLOPT_SSL_VERIFYPEER => 0,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => "GET",
-        ));
-        $short_data = curl_exec($curl);
+        ]);
+
+        $data = curl_exec($curl);
+
         curl_close($curl);
-        return $short_data;
+
+        return $data;
+
         //$ctx = stream_context_create(['https'=>[ 'timeout' => 3]]);
         //return @file_get_contents($url, false, $ctx);
     }
